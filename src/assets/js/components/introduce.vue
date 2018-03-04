12ec1afce74d43716ce03c81f3244a7d4c71d71a
‘surumebeer’ <06shuji09@gmail.com>
2018-03-05 06:17:32 +0900

    update

--- src/assets/js/components/introduce.vue
+++ src/assets/js/components/introduce.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    <ul>
+    <ul class="profile">
       <li>西野修史（Shuji Nishino）</li>
       <li>1989/6/9</li>
       <li>Frontend Engineer</li>
-      <li>HTML, CSS, Javascript, node.js, Vue.js</li>
+      <li>HTML, CSS, Javascript, node.js, Vue.js, Wordpress</li>
+    </ul>
+    <ul class="history">
+      <li>
+        <p>2013 ~ :</p>
+        <p>web制作会社にてマークアップエンジニアとしてWordpressのオリジナルテーマを用いたサイト構築・運用と、HTML, CSSでのマークアップを担当。</p>
+      </li>
+      <li>
+        <p>2016 ~ :</p>
+        <p>大規模求人サイトにてフロントエンドエンジニアとしてJavascriptを用いたサイトの開発、運用、A/Bテストを用いたUI/UX改善、SPサイトのリニューアルを担当</p>
+      </li>
+      <li>
+        <p>2017 ~ :</p>
+        <p>大規模クチコミサイトにて、フロントエンドエンジニアとして、node.jsを用いたgulp,webpackなど開発環境の管理、vue.jsを用いたUI/UXコンポーネントの作成などを担当</p>
+      </li>
     </ul>
   </div>
 </template>
@@ -16,5 +30,23 @@ export default {}
 <style lang="scss" scoped>
 li {
   line-height: 1.5;
+}
+
+.profile {
+  margin-bottom: 10px;
+}
+.history {
+
+  li {
+    margin-bottom: 10px;
+  }
+  p {
+    display: table-cell;
+    vertical-align: top;
+
+    &:first-child {
+      width: 70px;
+    }
+  }
 }
 </style>
--- src/assets/js/components/portfolio.vue
+++ src/assets/js/components/portfolio.vue
@@ -4,7 +4,7 @@
       <li v-for="item in list">
         <a v-if="item.url" :href="item.url" target="blank">{{item.name}}</a>
         <p v-else>{{item.name}}</p>
-        <a :href="item.github">github</a>
+        <a :href="item.github" target="blank">github</a>
       </li>
     </ul>
   </div>
