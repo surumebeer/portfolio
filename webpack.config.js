57aa78001b958188707f0e67810c606546c0b2d8
‘surumebeer’ <06shuji09@gmail.com>
2018-03-11 04:01:14 +0900

    add portfolio to portfolio

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,6 @@ module.exports = {
   module: {
     rules:[
       {
-        test: /\.json$/,
-        exclude: /node_modules/,
-        loader: 'json-loader'
-      },{
         test: /\.vue$/,
         loader: 'vue-loader',
       },
